57ac9ea8cc493e59ace032502360be289b55ae6c
nian0114 <268078545@qq.com>
2016-07-03 16:41:21 -0400

    bring slim

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -186,8 +186,8 @@ BOARD_USES_QC_TIME_SERVICES := true
 MALLOC_IMPL := dlmalloc
 
 # MKHW
-BOARD_HARDWARE_CLASS := device/xiaomi/leo/cmhw \
-     hardware/cyanogen/cmhw
+#BOARD_HARDWARE_CLASS := device/xiaomi/leo/cmhw \
+#     hardware/cyanogen/cmhw
 
 # Ril
 FEATURE_QCRIL_UIM_SAP_SERVER_MODE := true
